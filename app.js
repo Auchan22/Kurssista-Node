a5001195f8b2dcb94076ebb248e83c49c455ba01
Agustin <agustinsurila@gmail.com>
2022-03-22 22:25:51 -0300

    Se agregaron los metodos para eliminar a un usuario y obtenerlos a todos (Ejercicio 3.1 a 3.6)

--- app.js
+++ app.js
@@ -1,131 +1,91 @@
-// CLASE 1
-// // const http = require("http"); Clase 1
-
-// let notes = [
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2019-05-30T17:30:31.098Z",
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2019-05-30T18:39:34.091Z",
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2019-05-30T19:20:14.298Z",
-//     important: true,
-//   },
-// ];
-
-// const app = http.createServer((req, res) => {
-//   //   res.writeHead(200, { "Content-Type": "text/plain" }); Clase 1 Version 1
-//   res.writeHead(200, { "Content-Type": "application/json" }); /*Clase 1 Version 2 */
-//   res.end(JSON.stringify(notes));
-// });
-
-// const PORT = 3001;
-
-// app.listen(PORT);
-// console.log(`Tu app esta lista en http://localhost:${PORT}`);
-
-//CLASE 2
-const { response } = require("express");
 const express = require("express");
-const nodemon = require("nodemon");
+const API = require("./api/api.json");
 const app = express();
 
 app.use(express.json());
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
-//Ruta principal
-app.get("/", (req, res) => {
-  res.send("<h1>Hola world</h1>");
-});
-
-//Obteniendo todos los recursos
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
-});
-
-//Obteniendo un solo recurso
-app.get("/api/notes/:id", (req, res) => {
-  //   console.log(req.params);
-  const id = Number(req.params.id);
-  const note = notes.find((note) => note.id === id);
-  //   console.log(note);
-
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(400).end();
-  }
-});
-
-//Borrando un recurso, utilizando el metodo .delete()
-app.delete("/api/notes/:id", (req, res) => {
-  //   console.log(req.params);
-  const id = Number(req.params.id);
-  const note = notes.filter((note) => note.id !== id);
-  //   console.log(note);
-
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(400).end();
-  }
-});
-
-//Creando un recurso con el metodo post
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
+const getQuantPeople = () => {
+  return API.length;
 };
 
-app.post("/api/notes", (req, res) => {
-  // console.log(note);
-  // console.log(req.headers);
+let fechaActual = new Date();
 
+//Ruta principal
+app.get("/info", (req, res) => {
+  res.send(`
+  <div>
+  <h2>La lista de telefonos tiene informacion de ${getQuantPeople()} personas</h2>
+  <h2>${fechaActual}</h2>
+  </div>
+  `);
+});
+
+//Obteniendo todos los datos
+app.get("/api/persons", (req, res) => {
+  res.json(API);
+});
+
+//Obteniendo los datos de una persona en particular
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = API.find((p) => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res
+      .status(400)
+      .end({ error: "no se pudo encontrar el usuario con ese id" });
+  }
+});
+
+//Borrando los datos de una persona
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = API.filter((p) => p.id !== id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(400).end({ error: "no se pudo borrar el usuario con ese id" });
+  }
+});
+
+//Crear nuevos datos
+
+const generateId = () => {
+  return new Date().getTime();
+};
+
+app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  if (!body.content) {
-    return res.status(400).json({ error: "content missing" });
+  if (!body.name) {
+    return res.status(400).json({ error: "name missed" });
   }
 
-  const note = {
+  if (!body.number) {
+    return res.status(400).json({ error: "number missed" });
+  }
+
+  const repeated = API.find((p) => p.name === body.name);
+  // console.log(repeated);
+
+  if (repeated) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
+  const person = {
     id: generateId(),
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+    name: body.name,
+    number: body.number,
   };
 
-  notes = notes.concat(note);
+  API.push(person);
 
-  res.json(note);
+  res.json(person);
 });
 
 const PORT = 3001;

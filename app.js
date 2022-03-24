5af903279a921d2353dea0304a5926cea8e16a51
Agustin <agustinsurila@gmail.com>
2022-03-24 16:42:00 -0300

    Ejercicios 3.7 y 3.8 realizados utilizando "morgan"

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const API = require("./api/api.json");
 const app = express();
+const middleMorgan =
+  morgan("combined"); /*Ejercicio 3.7, usando una funcion predefinida */
+
+morgan.token("data", function getData(req) {
+  return JSON.stringify(req.body);
+});
+/* Este token permite obtener el body del metodo, y devolverlo para que puede mostrarlo en consola. Esto se puede ya que se creo un token particular */
+
+const middleMorgan2 = morgan(
+  `:method :url :status :res[content-length] - :response-time ms :data`,
+);
 
 app.use(express.json());
+app.use(middleMorgan2);
 
 const getQuantPeople = () => {
   return API.length;
@@ -61,6 +75,7 @@ const generateId = () => {
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
+  console.log(req.body);
 
   if (!body.name) {
     return res.status(400).json({ error: "name missed" });
